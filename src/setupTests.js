a38b1fc4dd4ffb9968585cac7a73476388b70bc4
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-10-20 18:40:50 +0200

    ad jsdom and polyfill

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,13 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
+import { JSDOM } from "jsdom";
+import { TextEncoder } from "util";
 
 import Enzyme from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+const dom = new JSDOM("<!doctype html><html><body></body></html>");
+global.document = dom.window.document;
+global.window = dom.window;
+
+global.TextEncoder = TextEncoder;
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
